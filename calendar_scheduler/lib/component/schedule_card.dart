11d92fee517e0d8b7189d7a30d80b252a2a9594e
0zine <0zine@naver.com>
2023-08-25 17:34:21 +0900

    일정 관리 앱 UI

---
+++ calendar_scheduler/lib/component/schedule_card.dart
@@ -0,0 +1,102 @@
+import 'package:calendar_scheduler/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class ScheduleCard extends StatelessWidget {
+
+  final int startTime;
+  final int endTime;
+  final String content;
+
+  const ScheduleCard({
+    required this.startTime,
+    required this.endTime,
+    required this.content,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1.0,
+          color: PRIMARY_COLOR,
+        ),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(6.0),
+        child: IntrinsicHeight(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _Time(  //시작과 종료 시간을 보여줄 위젯
+                startTime: startTime,
+                endTime: endTime
+              ),
+              SizedBox(width: 6.0),
+              _Content( //일정 내용을 보여줄 위젯
+                  content: content,
+              ),
+              SizedBox(width: 16.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Time extends StatelessWidget {
+  final int startTime;
+  final int endTime;
+
+  const _Time({
+    required this.startTime,
+    required this.endTime,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      fontWeight: FontWeight.w600,
+      color: PRIMARY_COLOR,
+      fontSize: 16.0,
+    );
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '${startTime.toString().padLeft(2, '0')}:00',
+          style: textStyle,
+        ),
+        Text(
+          '${endTime.toString().padLeft(2, '0')}:00',
+          style: textStyle.copyWith(
+            fontSize: 10.0,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Content extends StatelessWidget {
+  final String content;
+
+  const _Content({
+    required this.content,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Text(
+          content,
+        ),
+    );
+  }
+}
---
+++ calendar_scheduler/lib/screen/home_screen.dart
@@ -0,0 +1,73 @@
+import 'package:calendar_scheduler/component/main_calendar.dart';
+import 'package:calendar_scheduler/component/schedule_bottom_sheet.dart';
+import 'package:calendar_scheduler/component/schedule_card.dart';
+import 'package:calendar_scheduler/component/today_banner.dart';
+import 'package:calendar_scheduler/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: PRIMARY_COLOR,
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isDismissible: true,
+            isScrollControlled: true,
+            builder: (_) => ScheduleBottomSheet(),
+          );
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+
+      body: SafeArea(
+        child: Column(
+          children: [
+            //달력 위젯
+            MainCalendar(
+              selectedDate: selectedDate,
+              onDaySelected: onDaySelected,
+            ),
+            SizedBox(height: 8.0),
+            TodayBanner(
+                selectedDate: selectedDate,
+                count: 0,
+            ),
+            SizedBox(height: 8.0),
+            ScheduleCard(
+                startTime: 12,
+                endTime: 14,
+                content: 'English Study',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate){
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
+  }
+}
+
+
